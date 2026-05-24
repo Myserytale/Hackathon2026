86f2962109837c27d2a58e296255e4eb27208afa
Myserytale <laszlo.lengyel@stud.ubbcluj.ro>
2026-05-24 07:47:40 +0300

    fix: add findByRole to UserRepository to fix UserController compilation

--- backend/src/main/java/com/digitalromania/farm/repositories/UserRepository.java
+++ backend/src/main/java/com/digitalromania/farm/repositories/UserRepository.java
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmailIgnoreCase(String email);
+
+    List<User> findByRole(Role role);
 }
